28475a83f2c6be3aef34574411955c5a7f5762d5
Edwardcm07 <ingeduarcanomontoya@gmail.com>
2022-09-20 21:50:16 -0500

    first commit

---
+++ 3.taller3_condicionales_simples.py
@@ -0,0 +1,21 @@
+#TALLER 3 PYTHON
+#AUTOR:EDUAR ALEJANDRO CANO MONTOYA
+#FECHA: 20 DE SEPTIEMBRE DE 2022
+
+from datetime import date;
+hoy= date.today(); #fechaactual
+
+print("hoy es el dia ", hoy);
+
+a=int(input("digite el valor de A: "));
+b=int(input("digite el valor de B: "));
+
+if a >=b:
+    print("A es mayor o igual a B");
+else:
+    print("A es menor que B ");
+
+
+
+
+
